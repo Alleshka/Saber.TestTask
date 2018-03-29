31737e6c0b0a0ba6e683abeb8d5b35ea650b1010
grebovod_aleksei@mail.ru <grebovod_aleksei@mail.ru>
2018-03-29 12:20:04 +0300

    improve test

--- Saber.Serializer/Saber.Serializer.Test/UnitTest1.cs
+++ Saber.Serializer/Saber.Serializer.Test/UnitTest1.cs
@@ -54,6 +54,32 @@ namespace Saber.Serializer.Test
             // Asserst
             Assert.AreEqual(listRand1.Count, listRand2.Count);
             if (listRand1.Count > 0) Assert.AreEqual(listRand1.Head.Data, listRand2.Head.Data);
+
+            var cur1 = listRand1.Head;
+            var cur2 = listRand1.Head;
+
+            while ((cur1 != null) || (cur2 != null))
+            {
+                Assert.AreEqual(cur1.Data, cur2.Data);
+
+                if ((cur1.Prev != null) && (cur2.Prev != null))
+                {
+                    Assert.AreEqual(cur1.Prev.Data, cur2.Prev.Data);
+                }
+
+                if ((cur1.Next != null) && (cur2.Next != null))
+                {
+                    Assert.AreEqual(cur1.Next.Data, cur2.Next.Data);
+                }
+
+                if ((cur1.Rand != null) && (cur2.Rand != null))
+                {
+                    Assert.AreEqual(cur1.Rand.Data, cur2.Rand.Data);
+                }
+
+                cur1 = cur1.Next;
+                cur2 = cur2.Next;
+            }
         }
     }
 }
